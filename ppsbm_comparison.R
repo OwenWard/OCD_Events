dde3765f097c87c61bc21221f9316fa8ad16cc1f
Owen Ward <owengward@gmail.com>
2019-12-01 10:11:31 -0500

    simple simulation example

--- ppsbm_comparison.R
+++ ppsbm_comparison.R
@@ -9,25 +9,41 @@ true_Z = apply(generated_Q3_n20$z, 2, which.max)
 adjustedRandIndex(true_Z,est_Z)
 
 dim(results[[1]]$logintensities.ql.ij)
+#### simulate new data ####
+prop.groups <- c(0.5,0.5)
+
+# 3 different intensity functions :
+intens <- list(NULL)
+intens[[1]] <- list(intens= function(x) 100*x*exp(-8*x),max=5)
+# (q,l) = (1,1)
+intens[[2]] <- list(intens= function(x) exp(3*x)*(sin(6*pi*x-pi/2)+1)/2,max=13)
+# (q,l) = (1,2)
+intens[[3]] <- list(intens= function(x) 8.1*(exp(-6*abs(x-1/2))-.049),max=8)
+# (q,l) = (2,2)
+intens[[4]] <- list(intens= function(x) 1.1*(exp(-2*abs(x-1/2))-.1),max=3)
+
+obs <- generateDynppsbm(intens,Time=1,n=20,prop.groups,directed=T)
+
 
 ### fit this with our model also. reformat for this ####
 # need to use this and then match across
+# difficulty is this data being undirected
 n = 20
-index = listNodePairs(n,directed = F)
-Id = convertNodePair(index[,1],index[,2],n,directed = F)
+index = listNodePairs(n,directed = T)
+Id = convertNodePair(index[,1],index[,2],n,directed = T)
 Id 
 # so these correspond exactly in some sense
 
-n_events = length(generated_Q3_n20$data$time.seq)
+n_events = length(obs$data$time.seq)
 
-generated_Q3_n20$data$type.seq
+#generated_Q3_n20$data$type.seq
 
 alltimes_ppsbm = matrix(0,nrow = n_events,ncol = 3)
 for(i in 1:n_events){
-  ind = generated_Q3_n20$data$type.seq[i]
+  ind = obs$data$type.seq[i]
   alltimes_ppsbm[i,1] = index[ind,1]-1
   alltimes_ppsbm[i,2] = index[ind,2]-1
-  alltimes_ppsbm[i,3] = generated_Q3_n20$data$time.seq[i]
+  alltimes_ppsbm[i,3] = obs$data$time.seq[i]
 }
 
 alltimes_ppsbm # can then use this
@@ -56,13 +72,13 @@ for(i in 1:n){
 
 
 #####
-K = 3
+K = 2
 m = 20
 T = 1
 dT = 0.01
 Pi = rep(1/K,K)
 B = matrix(runif(K*K),K,K)
-diag(B) = rnorm(3,mean = 1, sd = 0.1)
+diag(B) = rnorm(2,mean = 1, sd = 0.1)
 tau = matrix(runif(m*K),nrow=m,ncol=K)
 tau = tau/rowSums(tau)
 S = matrix(0,nrow = m,ncol = K)
@@ -71,13 +87,20 @@ Mu = matrix(runif(K*K),K,K)
 
 
 results_online <- estimate_Poisson(full_data = alltimes_ppsbm,tau,B,Pi,S,A_pp,m,K,dT,T)
-results <- online_estimator(alltimes_ppsbm, A_pp, m, K, T, dT, lam = 1, B, Mu, tau)
+results_hawkes <- online_estimator(alltimes_ppsbm, A_pp, m, K, T, dT, lam = 1, B, Mu, tau)
 
-true_Z = apply(generated_Q3_n20$z,2,which.max)
+true_Z = apply(obs$z,2,which.max)
 
 est_Z_online_Poisson = apply(results_online$tau,1,which.max)
 adjustedRandIndex(est_Z_online_Poisson,true_Z)
 
 
-est_Z_online_H = apply(results$tau,1,which.max)
+est_Z_online_H = apply(results_hawkes$tau,1,which.max)
 adjustedRandIndex(est_Z_online_H,true_Z)
+
+
+results = mainVEM(data = obs$data, n = 20, Qmin = 2, Qmax = 2, method = 'kernel',
+                  directed = T,
+                  d_part = 1, n_perturb = 1)
+est_Z = apply(results[[1]]$tau, 2, which.max)
+adjustedRandIndex(true_Z,est_Z)
