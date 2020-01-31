7a14744dcc04ef67f5d6edb10d5c1be68b51ab2f
Owen Ward <owengward@gmail.com>
2020-01-31 14:57:12 -0500

    updates for paper, jan 31

--- College_Link_Prediction.R
+++ College_Link_Prediction.R
@@ -7,11 +7,14 @@ library(Rcpp)
 library(RcppArmadillo)
 sourceCpp("C:/Users/owenw/Documents/Online_Point_Process/onlineblock.cpp")
 
+set.seed(1)
+
+
 ### Read in raw data and preprocess to our format ####
 # this section uses dplyr
 library(tidyverse)
-college = read.csv(gzfile("C:/Users/owenw/Downloads/CollegeMsg.txt.gz"))
-#college = read.csv(gzfile("C:/Users/owenw/Downloads/sx-mathoverflow.txt.gz"))
+#college = read.csv(gzfile("C:/Users/owenw/Downloads/CollegeMsg.txt.gz"))
+college = read.csv(gzfile("C:/Users/owenw/Downloads/sx-mathoverflow.txt.gz"))
 # although we call the dataset college here in can be any dataset
 #college = read.csv(gzfile("C:/Users/owenw/Downloads/email-Eu-core-temporal.txt.gz"))
 
@@ -57,7 +60,7 @@ summary(college$Send)
 
 hist(college$Time)
 # this is all over the place in terms of email distribution also
-saveRDS(college,"college_messages.RDS")
+#saveRDS(college,"college_messages.RDS")
 
 
 #### analyse the data ####
@@ -89,24 +92,24 @@ A_test = lapply(A,unique)
 
 
 # check it fits on all data 
-Time = 804
-K = 2
-dT = 2
-Pi = rep(1/K,K)
-B = matrix(runif(K*K),K,K)
-Mu = matrix(runif(K*K),K,K)
-#diag(B) = rnorm(3,mean = 1, sd = 0.1)
-tau = matrix(runif(m*K),nrow=m,ncol=K)
-tau = tau/rowSums(tau)
-S = matrix(0,nrow = m,ncol = K)
-
-results_pois <- estimate_Poisson(full_data = as.matrix(college),tau,
-                            B,Pi,S,A_test,m,K,dT,T=Time)
-
-results_hawkes_sim <- online_estimator_eff(as.matrix(college), 
-                                           A_test, m, K, T = Time, dT, lam = 1, B, Mu, tau)
-
-results_hawkes_sim$Pi
+# Time = 804
+# K = 2
+# dT = 2
+# Pi = rep(1/K,K)
+# B = matrix(runif(K*K),K,K)
+# Mu = matrix(runif(K*K),K,K)
+# #diag(B) = rnorm(3,mean = 1, sd = 0.1)
+# tau = matrix(runif(m*K),nrow=m,ncol=K)
+# tau = tau/rowSums(tau)
+# S = matrix(0,nrow = m,ncol = K)
+# 
+# results_pois <- estimate_Poisson(full_data = as.matrix(college),tau,
+#                             B,Pi,S,A_test,m,K,dT,T=Time)
+# 
+# results_hawkes_sim <- online_estimator_eff(as.matrix(college), 
+#                                            A_test, m, K, T = Time, dT, lam = 1, B, Mu, tau)
+# 
+# results_hawkes_sim$Pi
 
 # split into training and test
 
@@ -120,16 +123,16 @@ summary(college$Time)
 
 # then do link prediction on the test set
 #train_time = 470.3
-train_time = 76.38
-#train_time = 1934
+#train_time = 76.38
+train_time = 1934
 #test_time = 804
-test_time = 193
-#test_time = 2351
+#test_time = 193
+test_time = 2351
 
 #### Hom Poisson ####
-dT = 0.5 # such that approx 400 windows for whole time period
-#dT = .25
-K = 2 # 2 for college, 4 for email
+dT = 20 # such that approx 400 windows for whole time period
+#dT = .5
+K = 3 # 2 for college, 4 for email, 3 for math
 Pi = rep(1/K,K)
 B = matrix(runif(K*K),K,K)
 Mu = matrix(runif(K*K),K,K)
@@ -138,35 +141,37 @@ tau = tau/rowSums(tau)
 S = matrix(0,nrow = m,ncol = K)
 
 # online estimate
-results_pois_train <- estimate_Poisson(full_data = as.matrix(college_train),tau,
-                                 B,Pi,S,A_test,m,K,dT,T=train_time)
+system.time(results_pois_train <- estimate_Poisson(full_data = as.matrix(college_train),tau,
+                                 B,Pi,S,A_test,m,K,dT,T=train_time) )
 
 # batch estimate for hom-Poisson needed here
-
+system.time(results_pois_batch <- batch_estimator_hom_Poisson(alltimes = as.matrix(college_train),
+                                                        A_test,m,K,T=train_time,B,tau,
+                                                        itermax = 100,stop_eps = 0.01))
 
 # simulate events
 est_Z = apply(results_pois_train$tau,1,which.max)
-B_Pois = matrix(0,nrow = K,ncol = K)
-pred_times = sampleBlockHak_pre(T = test_time,startT = train_time,
-                                A_test,est_Z-1,
-                                Mu = results_pois_train$B,B_Pois,lam=1)
-
-colnames(pred_times) = c("Send","Rec","Time")
-pred_times = as_tibble(pred_times)
-pred_set = pred_times %>% group_by(Send,Rec) %>% tally() %>%
-  filter(Send != Rec)
+# B_Pois = matrix(0,nrow = K,ncol = K)
+# pred_times = sampleBlockHak_pre(T = test_time,startT = train_time,
+#                                 A_test,est_Z-1,
+#                                 Mu = results_pois_train$B,B_Pois,lam=1)
+# 
+# colnames(pred_times) = c("Send","Rec","Time")
+# pred_times = as_tibble(pred_times)
+# pred_set = pred_times %>% group_by(Send,Rec) %>% tally() %>%
+#   filter(Send != Rec)
 test_set = college_test %>% group_by(Send,Rec) %>% tally()
 
 ### simulating events
-test_set %>% left_join(pred_set,by = c("Send","Rec")) %>%
-  mutate(n.y = replace_na(n.y,0)) %>%
-  rename(Pred_Count_Sim = n.y,
-         num_test = n.x) %>%
-  mutate(diff_sim = Pred_Count_Sim-num_test,
-         diff_zer = 0-num_test) %>%
-  ungroup() %>%
-  summarise(RMSE = sqrt(mean(diff_sim^2)),
-            RMSE_0 = sqrt(mean(diff_zer^2))) 
+# test_set %>% left_join(pred_set,by = c("Send","Rec")) %>%
+#   mutate(n.y = replace_na(n.y,0)) %>%
+#   rename(Pred_Count_Sim = n.y,
+#          num_test = n.x) %>%
+#   mutate(diff_sim = Pred_Count_Sim-num_test,
+#          diff_zer = 0-num_test) %>%
+#   ungroup() %>%
+#   summarise(RMSE = sqrt(mean(diff_sim^2)),
+#             RMSE_0 = sqrt(mean(diff_zer^2))) 
 
 
 # mean events
@@ -199,8 +204,8 @@ pred_test_set %>%
 
 
 ### Hom Hawkes ####
-K = 2
-dT = 0.5
+K = 3
+dT = 20
 Pi = rep(1/K,K)
 B = matrix(runif(K*K),K,K)
 Mu = matrix(runif(K*K),K,K)
@@ -210,39 +215,40 @@ tau = tau/rowSums(tau)
 S = matrix(0,nrow = m,ncol = K)
 
 # online estimator
-results_hawkes_sim <- online_estimator_eff(as.matrix(college_train), 
-                                           A_test, m, K, T = train_time, dT, lam = 1, B, Mu, tau)
+system.time(results_hawkes_sim <- online_estimator_eff(as.matrix(college_train), 
+                                           A_test, m, K, T = train_time, dT, 
+                                           lam = 1, B, Mu, tau))
 
 # batch estimator..
-results_hawkes_sim <- batch_estimator(as.matrix(college_train), 
+system.time(results_hawkes_sim <- batch_estimator(as.matrix(college_train), 
                                            A_test, m, K, T = train_time, dT, lam = 1, B, Mu, tau,
-                                      itermax =  1000,stop_eps = 0.01)
+                                      itermax =  100,stop_eps = 0.01))
 
 est_Z = apply(results_hawkes_sim$tau,1,which.max)
 
-pred_times = sampleBlockHak_pre(T = test_time,startT = train_time,A_test,est_Z-1,
-                                Mu = results_hawkes_sim$Mu,
-                                B = results_hawkes_sim$B,
-                                lam = results_hawkes_sim$lam)
-
-colnames(pred_times) = c("Send","Rec","Time")
-pred_times = as_tibble(pred_times)
-
-pred_set = pred_times %>% group_by(Send,Rec) %>% tally() %>% 
-  filter(Send!=Rec)
-
-
-
-#full_prediction using same as neurips 18 paper
-test_set %>% left_join(pred_set,by = c("Send","Rec")) %>%
-  mutate(n.y = replace_na(n.y,0)) %>%
-  rename(Pred_Count_Sim = n.y,
-         num_test = n.x) %>%
-  mutate(diff_sim = Pred_Count_Sim-num_test,
-         diff_zer = 0-num_test) %>%
-  ungroup() %>%
-  summarise(RMSE = sqrt(mean(diff_sim^2)),
-            RMSE_0 = sqrt(mean(diff_zer^2)))  
+# pred_times = sampleBlockHak_pre(T = test_time,startT = train_time,A_test,est_Z-1,
+#                                 Mu = results_hawkes_sim$Mu,
+#                                 B = results_hawkes_sim$B,
+#                                 lam = results_hawkes_sim$lam)
+# 
+# colnames(pred_times) = c("Send","Rec","Time")
+# pred_times = as_tibble(pred_times)
+# 
+# pred_set = pred_times %>% group_by(Send,Rec) %>% tally() %>% 
+#   filter(Send!=Rec)
+# 
+# 
+# 
+# #full_prediction using same as neurips 18 paper
+# test_set %>% left_join(pred_set,by = c("Send","Rec")) %>%
+#   mutate(n.y = replace_na(n.y,0)) %>%
+#   rename(Pred_Count_Sim = n.y,
+#          num_test = n.x) %>%
+#   mutate(diff_sim = Pred_Count_Sim-num_test,
+#          diff_zer = 0-num_test) %>%
+#   ungroup() %>%
+#   summarise(RMSE = sqrt(mean(diff_sim^2)),
+#             RMSE_0 = sqrt(mean(diff_zer^2)))  
 
 # mean number of events
 # this is very slow for the large network. will try make it faster
@@ -271,11 +277,16 @@ test_set %>% left_join(est,by = c("Send","Rec")) %>%
 window = 1/7
 K <- 3
 H <- 7
-dT = 10
+dT = 5
 MuA_start = array(0,c(K,K,H))
 tau_start = matrix(0,m,K)
-non_hom_pois_est <- nonhomoPois_estimator(as.matrix(college_train),A_test,m,K,H,window,
-                                          T = train_time,dT,gravity = 0.001,MuA_start,tau_start)
+system.time(non_hom_pois_est <- nonhomoPois_estimator(as.matrix(college_train),A_test,m,K,H,window,
+                                          T = train_time,dT,gravity = 0.001,
+                                          MuA_start,tau_start) )
+
+system.time(batch <- batch_nonhomoPois_estimator(as.matrix(college_train),A_test,m,K,H,window,T = train_time,dT,
+                            gravity = 0.001,MuA_start,tau_start,
+                            itermax = 100,stop_eps = 0.01 ))
 
 # taking the average of these basis functions for link prediction
 dim(non_hom_pois_est$MuA)
@@ -313,9 +324,19 @@ pred_test_set %>%
 
 
 B_start = matrix(0,K,K)
-non_homo_Hawkes_est = nonhomoHak_estimator_eff(as.matrix(college_train),A_test,m,K,H,window, lam = 1,
+system.time(non_homo_Hawkes_est <- nonhomoHak_estimator_eff(as.matrix(college_train),A_test,m,K,H,window, lam = 1,
                                                T = train_time,dT,gravity = 0.001,MuA_start = MuA_start,
-                                               tau_start = tau_start,B_start = B_start)
+                                               tau_start = tau_start,B_start = B_start) )
+
+## batch estimator
+system.time(non_homo_Hawkes_batch <- batch_nonhomoHak_estimator(as.matrix(college_train),A_test,m,K,H,window,T = train_time,dT,
+                                                    gravity = 0.001,MuA_start=MuA_start,
+                                                    tau_start= tau_start,lam = 1,
+                                                    B_start = B_start,
+                                                    itermax = 100,stop_eps = 0.01))
+
+
+
 non_homo_Hawkes_est$MuA
 non_homo_Hawkes_est$Pi
 non_homo_Hawkes_est$B
@@ -344,6 +365,10 @@ test_set %>% left_join(est,by = c("Send","Rec")) %>%
   summarise(RMSE = sqrt(mean(diff_mean^2)),
             RMSE_0 = sqrt(mean(diff_zer^2)))
 
+
+################
+################
+################
 ### Asymmetric CCRM Predictions ####
 
 W1 <- matrix(0,m,K)
