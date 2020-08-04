39d61d5195d4615dd938fc0861ac8d5eb63e29bf
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-08-04 15:20:24 -0400

    fit email data

--- data_analysis/explore_network_data.R
+++ data_analysis/explore_network_data.R
@@ -1,7 +1,7 @@
 library(tidyverse)
-library(Rcpp)
-library(RcppArmadillo)
-sourceCpp("cpp_files/onlineblock.cpp")
+#library(Rcpp)
+#library(RcppArmadillo)
+#sourceCpp("cpp_files/onlineblock.cpp")
 
 ### some other data ####
 fb_wall <- read_tsv(gzfile("data/facebook-wall.txt.gz"))
@@ -83,34 +83,107 @@ faculty_staff <- tidy_info %>%
 
 
 faculty_staff
-## we could use these known administrative departments as truth ##
-## then extract some of the emails
+##### extracting emails, using previous code ####
+data_path <- "C:/Users/owenw/Google Drive/Tian/EmailNetwork/Email-network CU/02/"
 
-emails <- read_lines(gzfile("C:/Users/owenw/Google Drive/Tian/EmailNetwork/Email-network CU/02/day001.gz"),
-                     skip_empty_rows = FALSE)
+files <- list.files(path = data_path)
 
-emails <- emails %>% str_split(pattern = ":")
+files
+# class 1 has 55 between 8 before
+#days = files[5:126]  # sem 1
+days = files[5:150]   # for sem 2
 
-# now a list entry for each element
-
-emails_tidy <- emails %>% purrr::map(function(.x) tibble(time =.x[1],
-                           send = .x[2],
-                           rec = .x[3],
-                           size = .x[4]))
-
-do.call(emails_tidy,bind_rows)
-
-em <- data.frame(emails_tidy)
-
-em <- do.call(rbind,emails_tidy)
-
-# need to remember how to split these
-
-em %>%
-  head() %>%
-  stringr::str_split(rec,pattern= ",")
-
-stringr::str_split(em$rec,pattern = ",")
+days_index <- c(1:146) # to account for one missing day
+days_index <- days_index[c(-13)] # missing for this semester, no data
 
 
-# then need to back this into the data we have
+output_total <- c()
+
+
+for(i in days_index){
+  print(i) 
+  day_sample <- readLines(gzfile(paste(data_path,days[i],sep = "")))
+  output <- str_split(day_sample,pattern = ":", simplify = T)
+  rec_num <- str_count(output[,3], pattern = ",")
+  # only keep emails with <5 recipients
+  temp <- output[which(rec_num<5),]
+  # then only keep senders who are faculty...
+  send <- which(temp[,2] %in% faculty_staff$uni)
+  # get recipients also
+  out <- str_split(temp[,3],pattern = ",")
+  n_obs <- 1 # max number of recipients considered.
+  seq.max <- seq_len(max(n_obs))
+  tmp = t(sapply(out, "[", i = seq.max))
+  if(n_obs == 1){
+    tmp = t(tmp)
+  }
+  indices = row(tmp)
+  rec <- indices[which(tmp %in% faculty_staff$uni)]
+  keep <- intersect(send,rec)
+  output_total <- rbind(output_total, temp[keep,])
+  
+}
+
+beepr::beep()
+
+# then only keep the first recipient of these emails
+
+all_recips <- output_total[,3]
+rec_all <- stringr::str_split(all_recips,pattern = ",",simplify = TRUE)
+first_recp <- rec_all[,1]
+
+events <- tibble(send = output_total[,2],recip = first_recp,
+                 Time = output_total[,1])
+
+events %>% 
+  group_by(send,recip) %>%
+  count() %>%
+  arrange(desc(n))
+  
+
+summary_events <- events %>% 
+  left_join(faculty_staff, by = c("send" = "uni")) %>%
+  left_join(faculty_staff, by = c("recip" = "uni")) %>%
+  group_by(send,recip) %>%
+  count() %>%
+  filter(n > 3)
+
+summary_events
+
+processed_events %>% ggplot(aes(Time_since)) + geom_histogram()
+
+(max(as.numeric(events$Time))-min(as.numeric(events$Time)))/147
+# corresponds to 1440 minutes in a day
+
+start <- min(as.numeric(events$Time))
+
+# this is in days now
+events %>% mutate(Time = as.numeric(Time) - start-1,
+                  Time = Time/1440 ) %>%
+  ggplot(aes(Time)) + geom_histogram()
+
+processed_events <- events %>% 
+  mutate(Time = as.numeric(Time)- (start - 10) ) %>%
+  mutate(Time = Time/1440) %>%
+  arrange(Time)
+
+### this is in days now, can use it almost now ###
+
+
+users <- unique(c(processed_events$send,processed_events$recip))
+m <- length(users)
+
+
+
+emails <- processed_events %>% 
+  mutate(Send = as.numeric(factor(send,levels = users))-1) %>% 
+  mutate(Rec = as.numeric(factor(recip,levels = users))-1) %>%
+  select(Send,Rec,Time)
+
+
+id_code <- faculty_staff %>% mutate(new_id = as.numeric(factor(uni,levels = users))-1) %>%
+  drop_na()
+# this gives the same number of users as m which is good
+
+
+### emails now in the format for algorithm
