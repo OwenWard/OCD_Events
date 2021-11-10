89a2ea90d6b936c8003cf39fe1bdb50a70987d35
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-11-09 21:12:09 -0500

    add function to compute loss in terms of predicted events

--- Experiments/analysis.R
+++ Experiments/analysis.R
@@ -1,4 +1,5 @@
 #### analyse the results of some simulation studies
+library(here)
 source(here("Experiments/utils.R"))
 
 ### EXP 1, investigate convergence in time ####
@@ -29,27 +30,6 @@ exp_1_results %>%
   geom_point()
 
 ## similarly, look at the ELBO metrics for these simulations
-exp_1_results <- exp_1_files %>% 
-  map(~readRDS(here("Experiments/exp_results/", .x))) %>% 
-  imap(~ .x %>% map_dfr(`[`, c("clust", "time", "est_elbo")) %>% 
-       mutate(sim = .y)) %>% 
-  bind_rows() %>% 
-  mutate(index = row_number())
-## not convinced this is in the format I want
-
-a <- readRDS(here("Experiments/exp_results/", exp_1_files[3])) 
-
-a %>% tibble(
-  clust = map_dbl(., "clust"),
-  elbo = map(., "est_elbo"),
-  time = map_dbl(., "time")#,
-  # wind = list(1:unique(time))
-) %>% 
-  mutate(sim = row_number()) %>% 
-  select(sim, clust, time, elbo) %>% 
-  unnest(cols = c(elbo)) %>% 
-  group_by(sim) %>% 
-  mutate(wind = 1:unique(time))
 
 # then the whole thing
 elbo_dat <- exp_1_files %>% 
@@ -71,11 +51,11 @@ elbo_dat %>%
   filter(time == 50) %>% 
   mutate(correct = ifelse(clust == 1, "Recover", "Don't")) %>% 
   ggplot(aes(wind, elbo, colour = correct)) + geom_line(aes(group = sim)) +
-  labs(x = "Windows", y = "Window ELBO") +
+  labs(x = "Windows", y = "Window ELBO", colour = "Recovery") +
   geom_hline(yintercept = -0.2)
 
 elbo_dat %>% 
-  filter(time == 500) %>% 
+  filter(time == 1000) %>% 
   filter(wind < 50) %>% 
   mutate(correct = ifelse(clust == 1, "Recover", "Don't")) %>% 
   ggplot(aes(wind, elbo, colour = correct)) + geom_line(aes(group = sim)) +
@@ -84,6 +64,91 @@ elbo_dat %>%
        title = "Comparison in initial optimisation",
        subtitle = "First 50 windows for T=500") +
   facet_wrap(~correct)
+
+## examine the recovery of the rate matrix also ##
+rate_exp_files <- list.files(path = here("Experiments/exp_results/"),
+                                      pattern = "exp1_store")
+
+
+rate_exp_files %>% 
+  map(~readRDS(here("Experiments/exp_results/", .x))) %>% 
+  flatten() %>% # to compress to a single list rather than nested
+  imap(~update_list(., sim = .y)) %>% 
+  map_dfr(`[`, c("sim","clust", "time", "est_B"))
+  
+b_data <- rate_exp_files %>% 
+  map(~readRDS(here("Experiments/exp_results/", .x))) %>% 
+  flatten() %>% # to compress to a single list rather than nested
+  imap(~update_list(., sim = .y))
+
+
+## then want this in the form of a data frame with a column for the
+## matrices
+## then some metric for the difference here, up to permutation
+b_df <- b_data %>% {
+  tibble(
+    map_dfr(., magrittr::extract, c("sim", "clust", "time"))
+  )
+} %>% 
+  mutate(sum_B = map_dbl(b_data, ~sum(.x$est_B)))
+## sum entries of each and abs of difference
+
+b_df %>% 
+  mutate(diff = abs(sum_B - 3.10)/4) %>% 
+  mutate(nodes = ifelse(clust == 1, "Recover", "Don't")) %>% 
+  ggplot(aes(nodes, diff)) +
+  geom_boxplot() +
+  labs(y = "Mean sum of Difference")
+
+b_df %>% 
+  mutate(diff = abs(sum_B - 3.10)/4) %>% 
+  mutate(nodes = ifelse(clust == 1, "Recover", "Don't")) %>% 
+  ggplot(aes(diff)) +
+  geom_histogram() +
+  facet_wrap(~nodes)
+  
+
+# this looks good, but the next step of interest is to look at the
+# rate of recovery of these plots over time.
+
+all_b_files <- list.files(path = here("Experiments/exp_results/"),
+                          pattern = "exp1_all")
+
+b_ests <- all_b_files %>% 
+  map(~readRDS(here("Experiments/exp_results/", .x))) %>% 
+  flatten() %>% 
+  imap(~update_list(., sim = .y))
+
+sum_b <- b_ests %>% map(~ apply(.x$est_B, 3, sum) )
+
+all_b_df <- b_ests %>% {
+  tibble(
+    map_dfr(., `[`, c("sim", "clust", "time")),
+    sum_B = sum_b
+  ) 
+} %>% 
+  unnest(sum_B) %>% 
+  mutate(diff = abs(sum_B - 3.1)/4) %>% 
+  group_by(sim) %>% 
+  mutate(window = row_number()) %>% 
+  ungroup()
+
+all_b_df %>% 
+  mutate(Cluster = ifelse(clust == 1, "Recover", "Don't"),
+         time = as.factor(time)) %>% 
+  # filter(time == 1000) %>% 
+  ggplot(aes(window, diff)) + 
+  geom_line(aes(group = sim, colour = Cluster)) +
+  facet_wrap(~time, scales = "free_x") +
+  labs(x = "Observation Period", y = "Difference in Rate",
+       subtitle = "78% of Random Initialisation Recover Communities Exactly",
+       title = "Recovery of Rate Matrix")
+
+all_b_df %>% 
+  mutate(ARI = ifelse(clust == 1, "Recover", "Don't")) %>% 
+  group_by(ARI, time) %>% 
+  distinct(sim) %>% tally()
+
 
 ### EXP 2:investigate relationship with dT ####
 
@@ -117,23 +182,34 @@ sims_df %>%
 
 
 #### EXP 3: Investigate theoretical regret rate ####
-exp_3_data <- readRDS(here("Experiments/exp_results/", "exp3.RDS"))
+exp_3_data <- readRDS(here("Experiments/exp_results/", "exp3_200.RDS"))
 
 exp3_tidy <- exp_3_data %>% tibble(
   sim = 1:100,
   ARI = map_dbl(., "clust"),
   regret = map(., "regret"),
-  dT = list(1:100)
+  dT = list(1:200)
 ) %>% 
   unnest(cols = c(regret, dT)) %>% 
   select(sim, ARI, regret, dT)
 
 exp3_tidy %>% 
-  mutate(ARI = ifelse(ARI == 1, "Recover Community", "Don't Recover")) %>% 
+  mutate(ARI = ifelse(ARI == 1, "Recover Community", "Don't Recover")) %>%
+  # filter(ARI )
+  filter(regret > 0) %>% 
   ggplot(aes(dT, regret)) +
-  geom_line(aes(group = sim), alpha = 0.25) +
-  stat_function(fun = function(x) sqrt(x) * log(x)^2, colour = "red") +
-  facet_wrap(~ARI) +
+  geom_line(aes(group = sim, colour = ARI), alpha = 0.4) +
+  stat_function(fun = function(x) 2*sqrt(x) * log(x*1000)^2,
+                colour = "black") +
+  facet_wrap(~ARI, scales = "free") +
   labs(y = "Regret Rate", x = "Windows",
        title = "Comparison to theoretical rate",
-       subtitle = "Ignoring the constants") 
+       subtitle = "Ignoring the constants") +
+  guides(colour = guide_legend(override.aes = list(alpha = 1)))
+
+exp3_tidy %>% 
+  group_by(sim) %>% 
+  filter(regret > 0) %>% 
+  group_by(ARI, sim) %>% 
+  tally() %>% 
+  filter(ARI == 1 & n == 200)
--- cpp_files/test.h
+++ cpp_files/test.h
@@ -16,6 +16,43 @@ Rcpp::List init_vals(int K, int n){
 }
 
 
+// [[Rcpp::export]]
+double curr_loss(
+    arma::mat curr_data,
+    arma::mat curr_B,
+    arma::vec true_z,
+    int K,
+    double dT,
+    int m) {
+  int n = curr_data.n_rows;
+  arma::mat event_counts(m,m), diff_mat(m,m);
+  // double total = 0;
+  
+  for(int i = 0; i < n; ++i) {
+    arma::rowvec event = curr_data.row(i);
+    int send = event(0);
+    int rec = event(1);
+    event_counts(send, rec) += 1;
+  }
+  // return event_counts;
+  // this is correct
+  // 
+  // cout<<"Here"<<endl;
+  for(int i = 0; i < m; ++i){
+    for(int j = 0; j < m; ++j){
+      if(i != j ){
+        int send_ind = true_z(i) - 1; // for zero index
+        int rec_ind = true_z(j) - 1;
+        double curr_rate = dT * curr_B(send_ind, rec_ind);
+        diff_mat(i,j) = event_counts(i,j) - curr_rate;
+      }
+    }
+  }
+  double norm_diff = accu(diff_mat)/ pow(m, 2); 
+  return abs(norm_diff).s;
+  // want to sum this and then divide by n^2?
+}
+
 
 // [[Rcpp::export]]
 Rcpp::List compute_regret(
@@ -49,6 +86,8 @@ Rcpp::List compute_regret(
   // int ind = 0;
   int nall = full_data.n_rows;
   arma::vec curr_elbo, ave_elbo, ave_ll, curr_ll, true_ll, av_true_ll;
+  arma::vec event_loss;
+  event_loss.zeros(N);
   curr_elbo.zeros(N);
   curr_ll.zeros(N);
   ave_ll.zeros(N);
@@ -87,10 +126,121 @@ Rcpp::List compute_regret(
     true_ll(n) = computeLL(elbo_dat, tau, true_B, Pi, A, m, K, t_curr);
     // was sub_data
     av_true_ll(n) = true_ll(n)/cum_events;
+    //
+    event_loss(n) = curr_loss(sub_data, curr_B, true_z, K, dT, m);
+    
   }
   return Rcpp::List::create(Named("EstLLH") = curr_ll,
                             Named("Ave_est_LLH") = ave_ll,
                             Named("TrueLLH") = true_ll,
-                            Named("Ave_true_LLH") = av_true_ll);
+                            Named("Ave_true_LLH") = av_true_ll,
+                            Named("Online_Loss")= event_loss);
 }
 
+
+
+// // [[Rcpp::export]]
+// double curr_loss(
+//   arma::mat curr_data,
+//   // arma::mat tau,/
+//   // arma::mat curr_B,
+//   // double dT,
+//   int K,
+//   int m
+// ){
+//   // construct event matrix and expected rate matrix
+//   arma::mat event_counts(m,m);
+//   event_counts.fill(0);
+//   // int num_events = curr_data.n_rows;
+//   // for(int i = 0; i < num_events; ++i){
+//   //   arma::rowvec event = curr_data.row(i);
+//   //   int send = event(0);
+//   //   int rec = event(1);
+//   //   event_counts(send, rec) += 1;
+//   // }
+//   //event_counts.print();
+//   //return Rcpp::List::create(Named("events") = event_counts);
+  // double diff = 1.0;
+  // return 1.0;
+// }
+
+
+// loss in terms of predicted events over time...
+// assuming known true z vectors say!
+// Rcpp::List compute_loss(
+//     arma::mat full_data, 
+//     Rcpp::List A, //arma::mat A,
+//     arma::mat tau,
+//     int m,
+//     int K,
+//     double T,
+//     double dT,
+//     arma::vec true_z,
+//     arma::cube B_ests,
+// ){
+//   // convert the true z vector to a tau matrix,
+//   // then can use compute llhood function
+//   arma::mat tau(m,K);
+//   tau.fill(0);
+//   // then iterate over z
+//   for(int i=0; i<m; ++i){
+//     int ind = true_z[i]-1;
+//     tau(i,ind) = 1;
+//   }
+//   arma::rowvec Pi;
+//   Pi = sum(tau, 0)/m;
+//   // Pi.print("Pi:");
+//   int N = int(T/dT);
+//   // double eta;
+//   int start_pos = 0;
+//   int curr_pos = 0;
+//   int end_pos = 0;
+//   // int ind = 0;
+//   int nall = full_data.n_rows;
+//   arma::vec curr_elbo, ave_elbo, ave_ll, curr_ll, true_ll, av_true_ll;
+//   curr_elbo.zeros(N);
+//   curr_ll.zeros(N);
+//   ave_ll.zeros(N);
+//   true_ll.zeros(N);
+//   av_true_ll.zeros(N);
+//   ave_elbo.zeros(N);
+//   int cum_events = 0;
+//   for(int n = 0; n < N; ++n){
+//     double Tn = dT*(n+1);
+//     arma::rowvec event = full_data.row(start_pos);
+//     double t_curr = event(2);
+//     while(t_curr <= Tn){
+//       if(curr_pos >= nall-1){
+//         break;
+//       }
+//       else{
+//         curr_pos += 1;
+//         event = full_data.row(curr_pos);
+//         t_curr = event(2);
+//       }
+//     }
+//     end_pos = curr_pos;
+//     arma::mat sub_data, elbo_dat;
+//     sub_data = full_data.rows(start_pos, end_pos);
+//     // then want to compare predicted events vs true, using sub_data
+//     
+//     cum_events += sub_data.n_rows;
+//     elbo_dat = full_data.rows(0,end_pos); 
+//     // this is the cumulative data, should it be just the events in that
+//     // window here?
+//     start_pos = curr_pos;
+//     // cout<<"here"<<endl;
+//     // R.printf();
+//     // Rcpp::print(B_ests.slice(n));
+//     arma::mat curr_B = B_ests.slice(n);
+//     curr_ll(n) = computeLL(elbo_dat, tau, curr_B, Pi, A, m, K, t_curr);
+//     ave_ll(n) = curr_ll(n)/cum_events;
+//     true_ll(n) = computeLL(elbo_dat, tau, true_B, Pi, A, m, K, t_curr);
+//     // was sub_data
+//     av_true_ll(n) = true_ll(n)/cum_events;
+//   }
+//   return Rcpp::List::create(Named("EstLLH") = curr_ll,
+//                             Named("Ave_est_LLH") = ave_ll,
+//                             Named("TrueLLH") = true_ll,
+//                             Named("Ave_true_LLH") = av_true_ll);
+// }
