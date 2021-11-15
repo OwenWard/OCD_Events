d518ecfadee51eca79c22d46d0bf0c44f4a924fd
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-11-15 18:42:07 -0500

    update exp 5 and exp 6

--- Experiments/Exp_6.R
+++ Experiments/Exp_6.R
@@ -1,5 +1,5 @@
-#### Experiments, Nov 15th
-#### Examining community recovery as the number of nodes is varied
+#### Experiment 6, Nov 15th
+#### Examining community recovery as the number of groups is varied
 
 .libPaths("/moto/stats/users/ogw2103/rpackages")
 
@@ -11,34 +11,46 @@ jobid <- Sys.getenv("SLURM_ARRAY_TASK_ID")
 jobid <- as.numeric(jobid)
 sim_id <- jobid
 
-node_vec <- c(50, 100, 200, 500, 1000)
+k_vec <- c(3:6)
 
-no_sims <- 20
-curr_n <- node_vec[sim_id]
+no_sims <- 50
+curr_k <- k_vec[sim_id]
+
+### need to form the intensity matrix here in a smart way
+form_intensity <- function(k, within_rates, between_rate) {
+  stopifnot(length(within_rates) == k)
+  B <- matrix(rep(between_rate, k*k), nrow = k, ncol = k)
+  diag(B) <- within_rates
+  return(as.vector(t(B)))
+}
+
+all_rates <- c(1:6)
+cur_rates <- all_rates[1:curr_k]
 
 
 results <- list()
 
 for(sim in 1:no_sims) {
   cat("Sim:", sim, "\n")
-  n <- curr_n
+  n <- 100
   Time <- 100
   intens1 <- c(2)
   intens2 <- c(1)
   true_B <- matrix(c(intens1, 0.05, 0.05, intens2), 
                    nrow = 2, ncol = 2, byrow = T)
   
-  intens <- matrix(c(intens1, 0.05, 0.05, intens2), 4, 1)
+  # intens <- matrix(c(intens1, 0.05, 0.05, intens2), 4, 1)
+  intens <- matrix(form_intensity(curr_k, cur_rates, 0.05), curr_k*curr_k, 1)
   sim1 <- generateDynppsbmConst(intens = intens,
                                 Time = Time,
                                 n = n, 
-                                prop.groups = c(0.5, 0.5))
+                                prop.groups = rep(1/curr_k, curr_k))
   proc_sim <- format_sims(sim_data = sim1, n = n)
   
-  K <- 2
+  K <- curr_k
   m <- n
   B <- matrix(runif(K * K), K, K)
-  dT <- 1
+  dT <- 1 #dT_vec[sim] # this should be current dT?
   # cat("dT:", curr_dT, "\n")
   inter_T <- 1
   
@@ -62,7 +74,7 @@ for(sim in 1:no_sims) {
     est_elbo = results_online$AveELBO,
     clust = clust_est,
     tau = results_online$tau,
-    curr_n = n
+    K = curr_k
   )
   results[[sim]] <- sim_pars
 }
@@ -71,4 +83,4 @@ for(sim in 1:no_sims) {
 
 saveRDS(results, file = here("Experiments",
                              "exp_results",
-                             paste0("exp5_", curr_n, ".RDS")))
+                             paste0("expk_", curr_k, ".RDS")))
