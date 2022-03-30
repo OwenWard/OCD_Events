4b742ff99c8c6e5959bad42f5fe6b2c9505e6461
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2022-03-30 10:53:41 -0400

    modify this to vary window size also

--- Experiments/OtherExps/Fig_1_test_1.R
+++ Experiments/OtherExps/Fig_1_test_1.R
@@ -67,129 +67,140 @@ for(sim in 1:no_sims){
   (sc_ari <- aricode::ARI(z_true, sc_est))
   ### this works
   
-  ### fit Pensky Zhang to this data
-  A_mats <- event_to_mat_seq(proc_sim$events,
-                             Total_time = Time, window_size = 10, n = n)
-  ## convert this to binary
-  A_mats[A_mats > 0] <- 1
+  possible_windows <- seq(from = 1, to = 10, by = 1)
+  for(wind in possible_windows){
+    print(wind)
+    print("-----")
+    ### fit Pensky Zhang to this data
+    A_mats <- event_to_mat_seq(proc_sim$events,
+                               Total_time = Time, window_size = wind, n = n)
+    ## convert this to binary
+    A_mats[A_mats > 0] <- 1
+    
+    l <- 4
+    m_pz <- 4
+    m0 <- 1
+    l0 <- 2
+    
+    A_pz <- pz_estimator_3(A = A_mats, time = Time,
+                           l0 = l0, m0 = m0,
+                           m = m_pz)
+    ### need to modify how this deals with changing the window
+    ### size here, maybe don't specify r at all?
+    
+    # A_pz
+    est_labels <- specc(A_pz, centers = 2)
+    (pz_ari <- aricode::ARI(z_true, est_labels@.Data))
+    
+    #### then our methods, for a given window size
+    #### Fit our method to this data
+    K <- 2
+    m <- n
+    B <- matrix(runif(K * K), K, K)
+    dT <- wind
+    inter_T <- 1
+    # capture output to not print out
+    results_online <- estimate_Poisson(full_data = events,
+                                       A = A_test,
+                                       m,
+                                       K,
+                                       Time,
+                                       dT,
+                                       B,
+                                       inter_T,
+                                       is_elbo = TRUE)
+    
+    # z_true <- apply(obs$z, 2, which.max)
+    z_est <- apply(results_online$tau, 1, which.max)
+    (clust_est <- aricode::ARI(z_true, z_est))
+    
+    ### makes sense Poisson not that good here.
+    
+    
+    ## Block Hawkes
+    Mu <- matrix(runif(K * K), K, K)
+    B <- matrix(runif(K * K), K, K)
+    tau <- matrix(1/K, nrow = m, ncol = K)
+    results_online_hawkes <- online_estimator_eff_revised(alltimes = events, 
+                                                          A = A_test,
+                                                          m,
+                                                          K,
+                                                          Time,
+                                                          dT = dT,
+                                                          lam = 1,
+                                                          B, Mu, tau,
+                                                          inter_T, 
+                                                          is_elbo = FALSE)
+    
+    z_est_haw <- apply(results_online_hawkes$tau, 1, which.max)
+    (haw_clust_est <- aricode::ARI(z_true, z_est_haw))
+    
+    ## Fitting Inhomogeneous Poisson
+    H <- 2
+    MuA <- array(runif(K * K * H), c(K, K, H))
+    window <- 0.5
+    tau <- matrix(1/K, nrow = m, ncol = K)
+    system.time(results_online_inpois <- nonhomoPois_estimator(alltimes = events,
+                                                               A = A_test,
+                                                               m,
+                                                               K,
+                                                               H,
+                                                               window,
+                                                               Time,
+                                                               dT,
+                                                               gravity = 0.001,
+                                                               MuA, tau))
+    
+    
+    (ari_in_pois <- aricode::ARI(apply(results_online_inpois$tau, 1, which.max),
+                                 z_true))
+    
+    ## Fitting inhomogeneous Hawkes
+    
+    window <- 1/2
+    K <- 2 # 4 for email, 2 for college, 3 for math
+    H <- 2
+    #### this was the bug#####
+    dT <- wind # 2 for email, 0.5 for college, 6 for math
+    MuA_start <- array(runif(K * K * H), c(K, K, H))
+    tau_start <- matrix(1/K, m, K)
+    B_start <- matrix(runif(K * K), nrow = K, ncol = K)
+    
+    result_inHaw <- nonhomoHak_estimator_eff_revised(alltimes = events,
+                                                     A = A_test,
+                                                     m,
+                                                     K,
+                                                     H,
+                                                     window,
+                                                     T = Time,
+                                                     dT,
+                                                     lam = 0.1,
+                                                     gravity = 0.0,
+                                                     B_start = B_start,
+                                                     MuA = MuA_start,
+                                                     tau = tau_start)
+    
+    
+    (ari_in_haw <- aricode::ARI(z_true, apply(result_inHaw$tau, 1, which.max)))
+    
+    ### then store all this
+    curr_sim_result <- tibble(ARI = c(pz_ari,
+                                      clust_est,
+                                      haw_clust_est,
+                                      ari_in_pois,
+                                      ari_in_haw),
+                              Method = c("PZ", "Poisson",
+                                         "Hawkes", "InPois", "InHawkes"),
+                              window_size = wind)
+    
+    all_results <- all_results %>% bind_rows(curr_sim_result)
+  }
   
-  l <- 4
-  m_pz <- 4
-  m0 <- 1
-  l0 <- 2
-  
-  A_pz <- pz_estimator_3(A = A_mats, time = Time,
-                         l0 = l0, m0 = m0,
-                         m = m_pz)
-  ### need to modify how this deals with changing the window
-  ### size here, maybe don't specify r at all?
-  
-  # A_pz
-  est_labels <- specc(A_pz, centers = 2)
-  (pz_ari <- aricode::ARI(z_true, est_labels@.Data))
-  ## this also works
-  
-  
-  #### Fit our method to this data
-  K <- 2
-  m <- n
-  B <- matrix(runif(K * K), K, K)
-  dT <- 1
-  inter_T <- 1
-  # capture output to not print out
-  results_online <- estimate_Poisson(full_data = events,
-                                     A = A_test,
-                                     m,
-                                     K,
-                                     Time,
-                                     dT,
-                                     B,
-                                     inter_T,
-                                     is_elbo = TRUE)
-  
-  # z_true <- apply(obs$z, 2, which.max)
-  z_est <- apply(results_online$tau, 1, which.max)
-  (clust_est <- aricode::ARI(z_true, z_est))
-  
-  ### makes sense Poisson not that good here.
-  
-  
-  ## Block Hawkes
-  Mu <- matrix(runif(K * K), K, K)
-  B <- matrix(runif(K * K), K, K)
-  tau <- matrix(1/K, nrow = m, ncol = K)
-  results_online_hawkes <- online_estimator_eff_revised(alltimes = events, 
-                                                        A = A_test,
-                                                        m,
-                                                        K,
-                                                        Time,
-                                                        dT = dT,
-                                                        lam = 1,
-                                                        B, Mu, tau,
-                                                        inter_T, 
-                                                        is_elbo = FALSE)
-  
-  z_est_haw <- apply(results_online_hawkes$tau, 1, which.max)
-  (haw_clust_est <- aricode::ARI(z_true, z_est_haw))
-  
-  ## Fitting Inhomogeneous Poisson
-  H <- 2
-  MuA <- array(runif(K * K * H), c(K, K, H))
-  window <- 0.5
-  tau <- matrix(1/K, nrow = m, ncol = K)
-  system.time(results_online_inpois <- nonhomoPois_estimator(alltimes = events,
-                                                             A = A_test,
-                                                             m,
-                                                             K,
-                                                             H,
-                                                             window,
-                                                             Time,
-                                                             dT,
-                                                             gravity = 0.001,
-                                                             MuA, tau))
-  
-  
-  (ari_in_pois <- aricode::ARI(apply(results_online_inpois$tau, 1, which.max),
-                              z_true))
-  
-  ## Fitting inhomogeneous Hawkes
-  
-  window <- 1/2
-  K <- 2 # 4 for email, 2 for college, 3 for math
-  H <- 2
-  #### this was the bug#####
-  dT <- 1 # 2 for email, 0.5 for college, 6 for math
-  MuA_start <- array(runif(K * K * H), c(K, K, H))
-  tau_start <- matrix(1/K, m, K)
-  B_start <- matrix(runif(K * K), nrow = K, ncol = K)
-  
-  result_inHaw <- nonhomoHak_estimator_eff_revised(alltimes = events,
-                                                   A = A_test,
-                                                   m,
-                                                   K,
-                                                   H,
-                                                   window,
-                                                   T = Time,
-                                                   dT,
-                                                   lam = 0.1,
-                                                   gravity = 0.0,
-                                                   B_start = B_start,
-                                                   MuA = MuA_start,
-                                                   tau = tau_start)
-  
-  
-  (ari_in_haw <- aricode::ARI(z_true, apply(result_inHaw$tau, 1, which.max)))
-  
-  ### then store all this
-  curr_sim_result <- tibble(ARI = c(sc_ari, pz_ari,
-                           clust_est,
-                           haw_clust_est,
-                           ari_in_pois,
-                           ari_in_haw),
-                           Method = c("Count", "PZ", "Poisson",
-                                      "Hawkes", "InPois", "InHawkes"))
-
-  all_results <- all_results %>% bind_rows(curr_sim_result)
+  ### add in the count ARI just here
+  count_sim <- tibble(ARI = sc_ari,
+                      Method = "Count",
+                      window_size = NA)
+  all_results <- all_results %>% bind_rows(count_sim)
 }
 
 
