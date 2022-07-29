c036387d335e7fc8ba46f1eebfa57cdafbb13c98
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2022-07-29 12:45:28 -0400

    fix a bug with block poisson where I was messing around with it before. working again now

--- cpp_files/blockPoisson.h
+++ cpp_files/blockPoisson.h
@@ -278,12 +278,12 @@ Rcpp::List estimate_Poisson(
 ){
   // iterate this over the time windows...
   int N = int(T/dT);
-  arma::cube inter_tau(1,1,1);
-  inter_tau.fill(1.0);
-  if(inter_T !=0){
+  // arma::cube inter_tau(1,1,1);
+  // inter_tau.fill(1.0);
+  // if(inter_T !=0){
     int slices = int(N/inter_T);
     arma::cube inter_tau(m,K,slices+1);
-  }
+  // }
   
   double eta;
   int start_pos = 0;
@@ -307,6 +307,7 @@ Rcpp::List estimate_Poisson(
   full_elbo.zeros(N);
   int cum_events = 0;
   for(int n = 0; n < N; ++n){
+    cout<<n<<endl;
     double Tn = dT*(n+1);
     arma::rowvec event = full_data.row(start_pos);
     double t_curr = event(2);
@@ -350,8 +351,8 @@ Rcpp::List estimate_Poisson(
   
   return Rcpp::List::create(Named("S")= S,
                             Named("tau")=tau,
-                            Named("early_tau")= inter_tau,
-                            Named("inter_B") = inter_B,
+                            // Named("early_tau")= inter_tau,
+                            // Named("inter_B") = inter_B,
                             Named("B")=B,
                             Named("Pi")=Pi,
                             Named("wind_elbo") = curr_elbo,
@@ -479,7 +480,6 @@ Rcpp::List estimate_Poisson_minimal(
   arma::mat tau(m,K);
   S.fill(1.0/K);
   tau.fill(1.0/K);
-  
   arma::vec curr_elbo, ave_elbo, ave_ll, curr_ll, full_elbo;
   curr_elbo.zeros(N);
   curr_ll.zeros(N);
@@ -490,34 +490,35 @@ Rcpp::List estimate_Poisson_minimal(
   for(int n = 0; n < N; ++n){
     cout<<n<<endl;
     double Tn = dT*(n+1);
-    double T0 = dT*n;
-    // arma::rowvec event = full_data.row(start_pos);
-    // double t_curr = event(2);
-    // while(t_curr <= Tn){
-    //   if(curr_pos >= nall-1){
-    //     break;
-    //   }
-    //   else{
-    //     curr_pos += 1;
-    //     event = full_data.row(curr_pos);
-    //     t_curr = event(2);
-    //   }
-    // }
+    // double T0 = dT*n;
+    arma::rowvec event = full_data.row(start_pos);
+    double t_curr = event(2);
+    while(t_curr <= Tn){
+      if(curr_pos >= nall-1){
+        break;
+      }
+      else{
+        curr_pos += 1;
+        event = full_data.row(curr_pos);
+        t_curr = event(2);
+      }
+    }
     ////
-    arma::vec times = full_data.col(2);
-    arma::uvec valid = find(times < Tn && times > T0);
-    arma::mat sub_data = full_data.rows(valid);
+    // arma::vec times = full_data.col(2);
+    // arma::uvec valid = find(times < Tn && times > T0);
+    // arma::mat sub_data = full_data.rows(valid);
     // cout<<curr_times.n_rows<<endl;
     // then just need to get the start of the indexing also, use that
     // to get the whole subset
-    ////
-    // end_pos = curr_pos;
-    // arma::mat sub_data, elbo_dat;
-    // sub_data = full_data.rows(start_pos, end_pos);
+    //
+    end_pos = curr_pos;
+    arma::mat sub_data, elbo_dat;
+    sub_data = full_data.rows(start_pos, end_pos);
     cum_events += sub_data.n_rows;
-    // elbo_dat = full_data.rows(0,end_pos); 
-    // start_pos = curr_pos;
+    elbo_dat = full_data.rows(0,end_pos);
+    start_pos = curr_pos;
     eta = 1/pow(1 + n, .5)/sub_data.n_rows*(K*K);
+    // cout<<dT<<endl;
     S = updateS(sub_data, tau, B, A, S, K, m, dT);
     tau = updateTau(S,Pi,m,K); 
     B = updateB(sub_data, tau, B, K, A, m, dT, eta);
--- cpp_files/init_Poisson.h
+++ cpp_files/init_Poisson.h
@@ -47,11 +47,14 @@ Rcpp::List estimate_Poisson_init(
   arma::cube inter_B(K,K,N);
   arma::cube inter_S(m, K, N);
   arma::vec curr_elbo, ave_elbo, ave_ll, curr_ll, full_elbo;
+  arma::vec cum_elbo, cum_events_vec;
   curr_elbo.zeros(N);
   curr_ll.zeros(N);
   ave_ll.zeros(N);
   ave_elbo.zeros(N);
   full_elbo.zeros(N);
+  cum_elbo.zeros(N);
+  cum_events_vec.zeros(N);
   int cum_events = 0;
   for(int n = 0; n < N; ++n){
     // cout<<n<<endl;
@@ -93,6 +96,8 @@ Rcpp::List estimate_Poisson_init(
       curr_ll(n) = computeLL(sub_data, tau, B, Pi, A, m, K, t_curr);
       ave_ll(n) = curr_ll(n)/cum_events;
       full_elbo(n) = computeELBO(full_data, tau, B, Pi, A, m, K, T);
+      cum_elbo(n) = computeELBO(elbo_dat, tau, B, Pi, A, m, K, t_curr)/cum_events;
+      cum_events_vec(n) = cum_events;
     }
     if(n % inter_T == 0 ){
       inter_tau.slice(ind) = tau;
@@ -111,7 +116,9 @@ Rcpp::List estimate_Poisson_init(
                             Named("AveELBO")=ave_elbo,
                             Named("wind_ll") = curr_ll,
                             Named("logL") = ave_ll,
-                            Named("full_ELBO") = full_elbo);
+                            Named("full_ELBO") = full_elbo,
+                            Named("Cum_ELBO") = cum_elbo,
+                            Named("Cum_Events") = cum_events_vec);
 }
 
 
