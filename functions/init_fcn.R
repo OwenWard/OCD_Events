22c490e3403d4025cc475b512e66a5220b93969f
OwenWard <owengward@gmail.com>
2022-11-03 16:32:32 -0700

    new init function for inhomogeneous Poisson in the sparse setting

--- functions/init_fcn.R
+++ functions/init_fcn.R
@@ -601,7 +601,6 @@ dense_inhom_Poisson <- function(alltimes, K, H, window, t_start, n0, m) {
     rename(clust_rec = clust)
   
   updated_Mu <- sorted_MuA
-  print(updated_Mu)
   t_end <- n0
   h1 <- floor(t_start/window) # start closest and to the left 
   h2 <- floor(t_end/window) # this will throw out incomplete intervals
@@ -635,20 +634,17 @@ dense_inhom_Poisson <- function(alltimes, K, H, window, t_start, n0, m) {
           select(starts_with("counts_H")) %>% 
           summarise_all(sum) %>% 
           as.numeric()
-        cat("total counts \n")
-        print(total_counts)
-        cat("----\n")
+        # cat("total counts \n")
+        # print(total_counts)
+        # cat("----\n")
         total_time <- new_data %>% select(starts_with("time_")) %>% 
           summarise_all(sum) %>% 
           as.numeric()
         # print(total_time)
         new_est <- total_counts/total_time
-        # print("now here")
-        print(new_est)
       }else{
         new_est <- runif(H)
       }
-      print(new_est)
       if(identical(new_est, numeric(0))){
         updated_Mu[k1, k2, ] <- 0
       }
@@ -675,3 +671,257 @@ dense_inhom_Poisson <- function(alltimes, K, H, window, t_start, n0, m) {
               cut_off = n0))
   
 }
+
+
+
+
+
+sparse_inhom_Poisson <- function(alltimes, K, H, window, t_start, n0, m, m0){
+  ###
+  ###
+  ### specify m0
+  ###
+  ### first get the event data to be used
+  ###
+  
+  tidy_events <- as_tibble(alltimes, 
+                           .name_repair = ~ c("V1", "V2", "V3")) %>% 
+    rename(send = V1, rec = V2, time = V3)
+  N <- nrow(tidy_events)
+  C <- 1
+  # n0 <- C * log(N)
+  init_events <- tidy_events %>% 
+    filter(time <= n0)
+  remaining_events <- alltimes[alltimes[,3]> n0, ]
+  
+  out_events <- init_events %>%
+    group_by(send) %>%
+    count() %>%
+    ungroup() %>%
+    rename(out = n)
+  
+  top_nodes <- out_events %>%
+    slice_max(out, n = m0, with_ties = FALSE) %>%
+    pull(send)
+  
+  center_matrix <- matrix(NA, nrow = length(top_nodes), ncol = K * H)
+  ### iterate along m0 nodes
+  # print("Getting this far")
+  for(i in seq_along(top_nodes)){
+    neighs <- init_events %>%
+      filter(send == top_nodes[i]) %>%
+      group_by(rec) %>%
+      summarise(events = list(time))
+    names(neighs$events) <- neighs$rec
+    ests <- map_dfr(neighs$events, fit_inpois, t_start = t_start,
+                    t_end = n0, window, H)
+    est_names <- paste0("H", 1:H)
+    ests <- as_tibble(t(ests), .name_repair = ~ c(paste0("V", 1:H)))
+    
+    if(nrow(unique(ests)) < K){
+      # print("Need to consider this case")
+      curr_est <- c(rep(0, H *K - K * nrow(unique(ests))), 
+                    as.vector(t(unique(ests))))
+      center_matrix[i, ] <- curr_est
+      ### pad with zeros to have length H*K but don't sort
+    } else if(nrow(unique(ests)) == K){
+      curr_est <- kmeans(ests, centers = K, algorithm = "Lloyd")
+      center_matrix[i, ] <- as.vector(t(curr_est$centers))
+    } else{
+      curr_est <- kmeans(ests, centers = K)
+      center_matrix[i, ] <- as.vector(t(curr_est$centers))
+    }
+    
+  }
+  
+  #### 
+  
+  ### then k means on center matrix
+  init_clust <- kmeans(center_matrix, centers = K)$cluster
+  
+  first_clust <- tibble(node = top_nodes,
+                        clust = init_clust)
+  
+  ### then get the events these nodes are involved in
+  ### should do this the same as previous method
+  init_clust_events <- init_events %>% 
+    filter(send %in% top_nodes) %>% 
+    filter(rec %in% top_nodes) %>% 
+    group_by(send, rec) %>% 
+    # count() %>% 
+    left_join(first_clust, by = c("send" = "node")) %>% 
+    rename(send_clust = clust) %>% 
+    left_join(first_clust, by = c("rec" = "node")) %>% 
+    rename(rec_clust = clust)
+  
+  init_MuA <- array(NA, dim = c(K, K, H))
+  t_end <- n0
+  h1 <- floor(t_start/window) # start closest and to the left 
+  h2 <- floor(t_end/window) # this will throw out incomplete intervals
+  jumps <- h1:h2 * window # to get the actual jumps
+  which_h <- rep(1:H, length.out = length(jumps) - 1)
+  
+  for(k1 in 1:K){
+    for(k2 in 1:K){
+      curr_data <- init_clust_events %>% 
+        filter(send_clust == k1) %>% 
+        filter(rec_clust == k2) %>% 
+        group_by(send, rec) %>% 
+        summarise(events = list(time)) %>% 
+        rowwise() %>% 
+        mutate(counts = list(num_in_wind(unlist(events),
+                                         jumps, which_h, 
+                                         window))) %>% 
+        ungroup()
+      if(nrow(curr_data) > 0){
+        new_data <- curr_data %>% 
+          unnest_wider(col = counts) %>% 
+          unnest_wider(col = c("counts_H", "time"), names_sep = "_")
+        ## then sum across counts and divide by all the time
+        # print(curr_data)
+        ## something happening here
+        
+        total_counts <- new_data %>% 
+          select(starts_with("counts_H")) %>% 
+          summarise_all(sum) %>% 
+          as.numeric()
+        # cat("total counts \n")
+        # print(total_counts)
+        # cat("----\n")
+        total_time <- new_data %>% select(starts_with("time_")) %>% 
+          summarise_all(sum) %>% 
+          as.numeric()
+        # print(total_time)
+        new_est <- total_counts/total_time
+      }else{
+        new_est <- runif(H)
+      }
+      
+      if(length(new_est) > 0) {
+        init_MuA[k1, k2, ] <- new_est
+      }
+      else{
+        init_MuA[k1, k2, ] <- 0
+      }
+    }
+  }
+  ### take all nodes not in top_nodes
+  rem_nodes <- tibble(
+    node = 0:(m-1),
+  ) %>% 
+    left_join(first_clust, by = "node") %>% 
+    filter(is.na(clust)) %>% 
+    pull(node)
+  
+  ord <- order(init_MuA[1, , 1])
+  sorted_MuA <- apply(init_MuA, c(1, 3), function(x) x[ord])
+  Mu_matrix <- matrix(as.vector(aperm(sorted_MuA,
+                                      c(3, 2, 1))),
+                      nrow = 2, ncol = 4, byrow = T)
+  
+  rem_group <- rep(NA, length(rem_nodes))
+  
+  for(i in seq_along(rem_nodes)) {
+    curr_neigh <- init_events %>% 
+      filter(send == rem_nodes[i]) %>% 
+      group_by(send, rec) %>% 
+      summarise(events = list(time))
+    
+    names(curr_neigh$events) <- curr_neigh$rec
+    ests <- map_dfr(curr_neigh$events, fit_inpois,
+                    t_start = t_start,
+                    t_end = n0, window, H)
+    est_names <- paste0("H", 1:H)
+    ests <- as_tibble(t(ests), .name_repair = ~ c(paste0("V", 1:H)))
+    
+    ####
+    if(length(nrow(unique(ests))) < K){
+      ### randomly assign to a cluster, if number distinct values < K
+      rem_group[i] <- sample(1:K, size = 1)
+    }else if(length(nrow(unique(ests))) == K){
+      ## use a different algorithm in this case
+      curr_est <- kmeans(ests, centers = K, algorithm = "Lloyd")
+      curr_center <- as.vector(t(curr_est$centers))
+      dists <- apply(Mu_matrix, 1, function(x) 
+        dist(rbind(x, curr_center)))
+      rem_group[i] <- which.min(dists)
+    }else{
+      curr_est <- kmeans(curr_neigh, centers = K)
+      curr_center <- as.vector(t(curr_est$centers))
+      dists <- apply(Mu_matrix, 1, function(x) 
+        dist(rbind(x, curr_center)))
+      rem_group[i] <- which.min(dists)
+    }
+  }
+  ### then update estimate of B
+  ### using full clusters
+  rem_clust <- tibble(node = rem_nodes,
+                      clust = rem_group)
+  all_clust <- bind_rows(first_clust, rem_clust)
+  
+  init_group <- all_clust %>% 
+    arrange(node) %>% 
+    pull(clust)
+  
+  ### the update the estimate of B
+  clust_events <- init_events %>% 
+    left_join(all_clust, by = c("send" = "node")) %>% 
+    rename(send_clust = clust) %>% 
+    left_join(all_clust, by = c("rec" = "node")) %>% 
+    rename(rec_clust = clust) 
+  
+  updated_MuA <- sorted_MuA
+  # print(updated_B)
+  for(k1 in 1:K){
+    for(k2 in 1:K){
+      curr_data <- clust_events %>% 
+        filter(send_clust == k1) %>% 
+        filter(rec_clust == k2) %>% 
+        group_by(send, rec) %>% 
+        summarise(events = list(time)) %>% 
+        rowwise() %>% 
+        mutate(counts = list(num_in_wind(unlist(events),
+                                         jumps, which_h, window))) %>% 
+        ungroup()
+      
+      if(nrow(curr_data) > 0) {
+        new_data <- curr_data %>% 
+          unnest_wider(col = counts) %>% 
+          unnest_wider(col = c("counts_H", "time"), names_sep = "_")
+        ## then sum across counts and divide by all the time
+        # print(curr_data)
+        ## something happening here
+        
+        total_counts <- new_data %>% 
+          select(starts_with("counts_H")) %>% 
+          summarise_all(sum) %>% 
+          as.numeric()
+        total_time <- new_data %>% select(starts_with("time_")) %>% 
+          summarise_all(sum) %>% 
+          as.numeric()
+        # print(total_time)
+        new_est <- total_counts/total_time
+      }else{
+        new_est <- runif(H)
+      }
+      if(identical(new_est, numeric(0))){
+        updated_Mu[k1, k2, ] <- 0
+      }
+      # else if(is.nan(new_est)){
+      #   updated_Mu[k1, k2, ] <- runif(1)
+      # }
+      else{
+        updated_Mu[k1, k2, ] <- new_est 
+      }
+    }
+  }
+  # print(updated_B)
+  
+  ### then return all this stuff in a list
+  return(list(est_clust = init_group,
+              est_Mu = updated_Mu,
+              rest_events = remaining_events,
+              cut_off = n0))
+}
+
+
