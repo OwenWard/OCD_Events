23c7d1eeed1bbf5f93676e8c40a41dbfaa20f579
OwenWard <owengward@gmail.com>
2023-09-26 15:37:30 -0700

    call the package that is needed

--- Experiments/Current_Experiments/Exp_InHom_Pois_dT.R
+++ Experiments/Current_Experiments/Exp_InHom_Pois_dT.R
@@ -7,7 +7,7 @@ library(here)
 
 source(here("functions/", "utils.R"))
 source(here("functions/init_fcn.R"))
-
+library(rbenchmark)
 
 ### simulate some data, see how the performance changes with the
 ### init function
